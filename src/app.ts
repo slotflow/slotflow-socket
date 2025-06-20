af2de6569cae8dd3f383e32ccf9b8b82f2fd9acc
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-06-20 19:07:50 +0530

    feat : Inital server setup

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import cors from 'cors';
+import helmet from 'helmet';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    allowedHeaders: ['Content-type', 'Authorization', 'Accept', 'X-Requested-with'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
+
+app.use(helmet());
+app.use(express.json({ limit:"50mb" }));
+app.use(express.urlencoded({ extended : true }));
+app.use(cookieParser());
+
+export default app;
