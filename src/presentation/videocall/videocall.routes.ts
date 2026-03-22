129415a958526156e7953c25ff00238db7df09ed
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2026-03-22 12:04:26 +0530

    video call handlers updated to connect video

--- src/presentation/videocall/videocall.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { videoCallController } from "./videocall.controller";
-import { authMiddleware } from "../middleware/auth.middleware";
-
-const router = Router();
-
-router.get("/:roomId",authMiddleware, videoCallController.joinCall);
-
-export default router;
